eb7647d0489948489f9a507be4b702ac123777ef
Nerzzz <guilhermeschiavolnsantos@gmail.com>
2026-04-07 10:53:37 -0300

    Mudanças no Cors e Express

--- server/server.js
+++ server/server.js
@@ -15,7 +15,16 @@ conn.on("error", (error) => console.error(error))
 conn.once("open", () => console.log("Open"))
 
 app.use(express.json())
-app.use(cors({origin:true, credentials:true}))
+app.use(cors({
+    origin:[
+        "http://localhost:5173",
+        "https://arrendei.vercel.app"
+    ],
+    credentials: true
+}))
+
+app.use(express.json({ limit: "10mb" }))
+app.use(express.urlencoded({ limit: "10mb", extended: true }))
 
 const userRouter = require("./routes/users")
 app.use("/users", userRouter)
